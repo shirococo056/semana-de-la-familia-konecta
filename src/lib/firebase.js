1bda6577d5a0d84b47805e75244e22bfb097d3d1
shirococo056 <91107435+shirococo056@users.noreply.github.com>
2021-10-07 18:18:37 -0300

    antes andaba, ahora pruebo arreglar errores

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,27 +1,28 @@
 /* eslint-disable prefer-const */
-import Firebase from 'firebase/app';
+import { initializeApp, Firebase } from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/storage';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from 'firebase/analytics';
+import { seedDatabase } from '../seed';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCFHuM-dhUN45L2yCWE-Z8L6aWryPk098A",
-  authDomain: "konecta-instagram.firebaseapp.com",
-  databaseURL: "https://konecta-instagram-default-rtdb.firebaseio.com",
-  projectId: "konecta-instagram",
-  storageBucket: "konecta-instagram.appspot.com",
-  messagingSenderId: "670817656287",
-  appId: "1:670817656287:web:a9bee5a4251033a7a9f86a",
-  measurementId: "G-HC0D29WZKG"
+// replace these with your own config values from Firebase!
+const config = {
+  apiKey: 'AIzaSyCFHuM-dhUN45L2yCWE-Z8L6aWryPk098A',
+  authDomain: 'konecta-instagram.firebaseapp.com',
+  databaseURL: 'https://konecta-instagram-default-rtdb.firebaseio.com',
+  projectId: 'konecta-instagram',
+  storageBucket: 'konecta-instagram.appspot.com',
+  messagingSenderId: '670817656287',
+  measurementId: 'G-HC0D29WZKG',
+  appId: '1:670817656287:web:a9bee5a4251033a7a9f86a'
 };
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
 
+seedDatabase(firebase);
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(config);
 const analytics = getAnalytics(app);
+
+export { app, firebase, FieldValue };
